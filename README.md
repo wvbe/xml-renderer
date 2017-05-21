332198fd8619e2e472c449af1ec612fdbbda2561
wvbe <wybe@x-54.com>
2017-05-22 00:20:41 +0200

    Updated example simple.js + patch version bump

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # xml-renderer
 
-A quite simple way of registering rendering functions for XML nodes, effectively an XSL with javascript functions. Works great transforming XML to React components, but writing to a JsonML structure or string is also fine.
+A quite simple way of registering rendering functions for XML nodes, effectively an XSL with javascript functions. Works
+great transforming XML to React components, but writing to a JsonML structure or string is also fine.
 
 ## To install
 
@@ -16,7 +17,9 @@ Assume a `const xr = new XmlRenderer()` shared between the following examples.
 
 ## Registering and rendering
 
-The first parameter to registering a renderer for a node is always an XPath boolean test. The second parameter is a callback that is expected to return the result for any XML node that matches the test. This callback is given an instance of `NodeRenderer` that allows you to access the actual XML node, or traverse into its children.
+The first parameter to registering a renderer for a node is always an XPath boolean test. The second parameter is a
+callback that is expected to return the result for any XML node that matches the test. This callback is given an
+instance of `NodeRenderer` that allows you to access the actual XML node, or traverse into its children.
 
 ```js
 // Render every <horizontal-ruler> XML element to an HTML <hr />. This is probably the simplest transformation.
@@ -33,7 +36,9 @@ identifier={ renderer.getNode().getAttribute('user-id') }
 />);
 ```
 
-Usually you'll start rendering of root nodes, or rendering of child nodes, using `NodeRenderer#traverse()`. Most of the time you'll actually be rendering a child element, so the `traverse()` call in a parent element's renderer is very important.
+Usually you'll start rendering of root nodes, or rendering of child nodes, using `NodeRenderer#traverse()`. Most of the
+time you'll actually be rendering a child element, so the `traverse()` call in a parent element's renderer is very
+important.
 
 ```js
 // Render every <paragraph> XML node to a regular <p> HTML element
@@ -42,21 +47,21 @@ xr.register('self::paragraph', renderer => <p key={ renderer.key() }>
 	{ renderer.traverse() }
 </p>);
 
-// Render a <webpage> XML node with a second traversal for all its <footnote> descendants
+// Render footnotes as an asterisk, but render them again as list items (+ children) somewhere else.
+xr.register('self::footnote', renderer => '*');
+xr.mode('footnote-render-mode').register('self::footnote', renderer => <li key={ renderer.key() }>
+	{ renderer.traverse() }
+</li>);
+
+// Render a <webpage> XML node with a second traversal in a different mode for all its <footnote> descendants
 //     in an ordered list under a horizontal ruler.
 xr.register('self::webpage', renderer => <div key={ renderer.key() }>
 	{ renderer.traverse() }
 	<hr />
 	<ol>
 		{ renderer.traverse('.//footnote', 'footnote-render-mode') }
-</ol>
+	</ol>
 </div>);
-
-// Render footnotes as an asterisk, but render them again as list items (+ children) somewhere else.
-xr.register('self::footnote', renderer => '*');
-xr.mode('footnote-render-mode').register('self::footnote', renderer => <li key={ renderer.key() }>
-	{ renderer.traverse() }
-</li>);
 ```
 
 To finally engage all of the above for an XML string that you've loaded from anywhere, use a `DOMParser` to create a DOM out of it and start `xr` for that.
@@ -84,7 +89,7 @@ ReactDOM.render(
 
 This should yield a DOM like this:
 
-```
+```xml
 <div>
 	<UserBadgeComponent identifier="wvbe" />
 	<p>The preceding element, "author", was not specifically mentioned in this example yet, but it matches the XPath test for 'self::*[@user-id]'.</p>
@@ -98,3 +103,5 @@ This should yield a DOM like this:
 	</ol>
 </div>
 ```
+
+The full code for this example is in [example/simple.js](./example/simple.js).
--- example/simple.js
+++ example/simple.js
@@ -3,63 +3,75 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import XmlRenderer from 'xml-renderer';
 
-const registry = new XmlRenderer();
+const UserBadgeComponent = ({ identifier }) => <example-user-badge>{ identifier }</example-user-badge>;
 
-// Register render functions for specific elements
-registry.register('self::text()', renderer => renderer.getNode().nodeValue);
+const xr = new XmlRenderer();
 
-registry.register('self::section', renderer => (
-	<div key={ renderer.key() }>
-		<h1>My first section</h1>
-		{ renderer.traverse() }
-		<hr />
-		<ol>
-			{ renderer.traverse('.//footnote', 'footnotes') }
-		</ol>
-	</div>
-));
+// Render every <horizontal-ruler> XML element to an HTML <hr />. This is probably the simplest transformation.
+xr.register('self::horizontal-ruler', renderer => <hr key={ renderer.key() } />);
 
-registry.register('self::paragraph', renderer => (
-	<p key={ renderer.key() }>
-		{ renderer.traverse() }
-	</p>
-));
+// Render text as text :) This is actually also a pretty simple transformation
+xr.register('self::text()', renderer => renderer.getNode().nodeValue);
 
-registry.register('self::paragraph[@special]', renderer => (
-	<p key={ renderer.key() }>
-		~{ renderer.traverse() }~
-	</p>
-));
+// Render every XML element that has a "user-id" attribute to a component called "UserBadgeComponent",
+//     and pass it the user identifier that we can get from the XML node.
+xr.register('self::*[@user-id]', renderer => <UserBadgeComponent
+	key={ renderer.key() }
+	identifier={ renderer.getNode().getAttribute('user-id') }
+/>);
 
-registry.register('self::footnote', renderer => <b key={ renderer.key() }>*</b>);
-registry.mode('footnotes').register('self::footnote', renderer => (
-	<li key={ renderer.key() }>
-		{ renderer.traverse() }
-	</li>
-));
 
-// Load an XML string from anywhere, and use the browser to create a DOM
-const xml = new window.DOMParser().parseFromString(`
-    <section>
-        <paragraph>My first paragraph<footnote>This is the footnote.</footnote>.</paragraph>
-        <paragraph>My second paragraph.</paragraph>
-        <paragraph special="true">My special paragraph.</paragraph>
-    </section>
-`, `application/xml`);
+// Render every <paragraph> XML node to a regular <p> HTML element
+//     and traverse to render its children.
+xr.register('self::paragraph', renderer => <p key={ renderer.key() }>
+	{ renderer.traverse() }
+</p>);
+
+// Render a <webpage> XML node with a second traversal for all its <footnote> descendants
+//     in an ordered list under a horizontal ruler.
+xr.register('self::webpage', renderer => <div key={ renderer.key() }>
+	{ renderer.traverse() }
+	<hr />
+	<ol>
+		{ renderer.traverse('.//footnote', 'footnote-render-mode') }
+	</ol>
+</div>);
+
+// Render footnotes as an asterisk, but render them again as list items (+ children) somewhere else.
+xr.register('self::footnote', renderer => '*');
+xr.mode('footnote-render-mode').register('self::footnote', renderer => <li key={ renderer.key() }>
+	{ renderer.traverse() }
+</li>);
+
+// Load an XML string from anywhere
+const xmlString = `
+	<webpage>
+		<author user-id="wvbe" />
+		<paragraph>The preceding element, "author", was not specifically mentioned in this example yet, but it matches the XPath test for 'self::*[@user-id]'.</paragraph>
+		<horizontal-ruler />
+		<paragraph>Footnotes will be rendered as an asterisk<footnote><paragraph>But the full footnote is rendered at the bottom of a page</paragraph></footnote></paragraph>
+	</webpage>
+`;
+
+const xmlDom = new window.DOMParser().parseFromString(xmlString, 'application/xml');
 
 // Render that baby
 ReactDOM.render(
-	<div>{ registry.node(xml).traverse() }</div>,
+	<div>{ xr.node(xmlDom).traverse() }</div>,
 	document.getElementById('root')
 );
 
-// <div>
-//     <h1>My first section</h1>
-//     <p>My first paragraph<b>*</b>.</p>
-//     <p>My second paragraph.</p>
-//     <p>~My special paragraph.~</p>
-//     <hr />
-//     <ol>
-//         <li>This is the footnote.</li>
-//     </ol>
-// </div>
+// Output:
+//	<div>
+//		<UserBadgeComponent identifier="wvbe" />
+//		<p>The preceding element, "author", was not specifically mentioned in this example yet, but it matches the XPath test for 'self::*[@user-id]'.</p>
+//		<hr />
+//		<p>Footnotes will be rendered as an asterisk*</p>
+//		<hr />
+//		<ol>
+//			<li>
+//				<p>But the full footnote is rendered at the bottom of a page</p>
+//			</li>
+//		</ol>
+//	</div>
+
