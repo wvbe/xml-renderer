294bf70cbe3f1144f26381106f06f2d2d531de44
wvbe <wybe@x-54.com>
2018-09-04 13:25:47 +0200

    Fix accumulating renderData on traversal

--- test/Experience.renderData.test.js
+++ test/Experience.renderData.test.js
@@ -8,32 +8,39 @@ basicExperience.register('self::node()', ({ traverse }) => traverse());
 basicExperience.register('self::text()', ({ key, node }) => node().nodeValue);
 
 describe('renderData', () => {
-	xtest('should accumulate as it is passed down to traversals', () => {
+	test('should accumulate as it is passed down to traversals', () => {
 		const experience = new Experience(basicExperience);
 
-		experience.register('self::x', ({ key, traverse, data1, data2 }) => [
-			'x',
-			data1,
-			data2,
-			...traverse({
-				// data1 should be passed even if it is not mentioned here
-				data2: data2 + 1
-			})
-		]);
-
-		experience.register('self::div', ({ key, traverse, data1, data2 }) => [
-			'div',
-			data1,
-			data2,
+		experience.register('self::element()', ({ key, traverse, node, query, ...renderData }) => [
+			node().nodeName,
+			renderData,
 			...traverse()
 		]);
 
-		const result = experience.render(sync(`<div><x><x /><div /></x><div /></div>`), {
+		experience.register('self::a', ({ traverse, node, data2 }) => traverse({
+			data2: data2 + 1
+		}));
+
+		experience.register('self::b', ({ traverse, node }) => traverse({
+			data3: 'beer'
+		}));
+
+		experience.register('self::c', ({ traverse, node }) => traverse({
+			data4: 'eggs'
+		}));
+
+		const result = experience.render(sync(`<a><b><with-beer /></b><c><with-eggs /></c></a>`), {
+			// Should exist on the deepest traversal level, even if it is never passed explicitly
 			data1: 'bacon',
-			data2: 0
+
+			// Accumulated by every traversal into an <a /> node
+			data2: 0,
+
+			// data3, Is only set when traversing the children of <b />
+
+			// data4, Is only set when traversing the children of <c />
 		});
 
-		console.log(result);
 		expect(result)
 			.toMatchSnapshot();
 	});
