5e37597ae50536233b2ae738c9d7d2a92503ccbf
wvbe <wybe@x-54.com>
2020-04-19 11:17:23 +0200

    Updates

---
+++ precache-manifest.fc6be144360cf0ab6519238839245fd1.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b4a7efa4d289b7d827cbd40499292f1d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8c285a7e2b09be914bb7",
+    "url": "/static/css/main.0eabfc94.chunk.css"
+  },
+  {
+    "revision": "6d1a27c95f3743a84906",
+    "url": "/static/js/2.c30f97b6.chunk.js"
+  },
+  {
+    "revision": "af353daab0d4400426b150fde4e7366d",
+    "url": "/static/js/2.c30f97b6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8c285a7e2b09be914bb7",
+    "url": "/static/js/main.dba66d18.chunk.js"
+  },
+  {
+    "revision": "3bdd7f9a25c1ce9d222e",
+    "url": "/static/js/runtime-main.41e3f362.js"
+  },
+  {
+    "revision": "4424dd2437702f0a265eb01802f3385b",
+    "url": "/static/media/playshakespeare.macbeth.4424dd24.xml"
+  },
+  {
+    "revision": "d3953046c3e5a8f7457cf5b5ff3b581b",
+    "url": "/static/media/playshakespeare.midsummernightsdream.d3953046.xml"
+  },
+  {
+    "revision": "865e8c7014c6a4b651b5f6d8a44a585a",
+    "url": "/static/media/playshakespeare.romeoandjuliet.865e8c70.xml"
+  },
+  {
+    "revision": "4a559c919482562e9e0d25a6345f60dc",
+    "url": "/static/media/playshakespeare.tempest.4a559c91.xml"
+  },
+  {
+    "revision": "17ca0aae11ae6b0f92f4d4877ad91370",
+    "url": "/static/media/w3cspec.xml.17ca0aae.xml"
+  },
+  {
+    "revision": "b80a96e26f704acce81c9c535f9399af",
+    "url": "/static/media/w3cspec.xpath.b80a96e2.xml"
+  },
+  {
+    "revision": "1b1b3f0af6716c9da8b6c2e7047d970b",
+    "url": "/static/media/w3cspec.xquery.1b1b3f0a.xml"
+  },
+  {
+    "revision": "b10eb7001a511f84001673a7119dfa76",
+    "url": "/static/media/w3cspec.xquf.b10eb700.xml"
+  }
+]);
