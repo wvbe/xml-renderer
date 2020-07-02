3d6547a414c35bfbcc0c5c744cbec8e4cd051699
wvbe <wybe@x-54.com>
2020-07-02 20:40:49 +0200

    Updates

---
+++ precache-manifest.9cfdf331900eff67b4f6ca50160c7eb8.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9617336c1f70d4f3059dde88d04119f6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d6d4af32c1f4272fc7b5",
+    "url": "/static/css/main.0eabfc94.chunk.css"
+  },
+  {
+    "revision": "4206e4bc471036beb83c",
+    "url": "/static/js/2.f67e53ec.chunk.js"
+  },
+  {
+    "revision": "af353daab0d4400426b150fde4e7366d",
+    "url": "/static/js/2.f67e53ec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d6d4af32c1f4272fc7b5",
+    "url": "/static/js/main.2fc345e3.chunk.js"
+  },
+  {
+    "revision": "3bdd7f9a25c1ce9d222e",
+    "url": "/static/js/runtime-main.41e3f362.js"
+  },
+  {
+    "revision": "33da26c00e8f07b02a9dec19bb242636",
+    "url": "/static/media/playshakespeare.macbeth.33da26c0.xml"
+  },
+  {
+    "revision": "d3953046c3e5a8f7457cf5b5ff3b581b",
+    "url": "/static/media/playshakespeare.midsummernightsdream.d3953046.xml"
+  },
+  {
+    "revision": "865e8c7014c6a4b651b5f6d8a44a585a",
+    "url": "/static/media/playshakespeare.romeoandjuliet.865e8c70.xml"
+  },
+  {
+    "revision": "58148a3431bb78bdc96a4af3223ae836",
+    "url": "/static/media/playshakespeare.tempest.58148a34.xml"
+  },
+  {
+    "revision": "17ca0aae11ae6b0f92f4d4877ad91370",
+    "url": "/static/media/w3cspec.xml.17ca0aae.xml"
+  },
+  {
+    "revision": "b80a96e26f704acce81c9c535f9399af",
+    "url": "/static/media/w3cspec.xpath.b80a96e2.xml"
+  },
+  {
+    "revision": "1b1b3f0af6716c9da8b6c2e7047d970b",
+    "url": "/static/media/w3cspec.xquery.1b1b3f0a.xml"
+  },
+  {
+    "revision": "b10eb7001a511f84001673a7119dfa76",
+    "url": "/static/media/w3cspec.xquf.b10eb700.xml"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.5dbf6677ba67e11002c13605644e7a22.js"
+  "/precache-manifest.9cfdf331900eff67b4f6ca50160c7eb8.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
